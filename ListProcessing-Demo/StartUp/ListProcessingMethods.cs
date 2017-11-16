61384dd280a9cce6d2478f3c6cb8f792feeda3be
svetliub <svet1981@abv.bg>
2017-11-16 11:32:10 +0200

    Added solution files to the project. Added append method to the solution.

---
+++ ListProcessing-Demo/StartUp/ListProcessingMethods.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace StartUp
+{
+    public class ListProcessingMethods
+    {
+        public static void Append(string word, List<string> list)
+        {
+            list.Add(word);
+            Console.WriteLine(string.Join(" ", list));
+        }
+    }
+}
---
+++ ListProcessing-Demo/StartUp/StartUp.cs
@@ -0,0 +1,54 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace StartUp
+{
+    public class StartUp
+    {
+        static void Main(string[] args)
+        {
+            List<string> stringList = Console.ReadLine().Split().ToList();
+
+            Console.WriteLine(string.Join(" ", stringList));
+
+            string input = Console.ReadLine();
+
+            while (!input.Equals("end"))
+            {
+                List<string> inputList = input.Split().ToList();
+                
+                string command = inputList[0];
+
+                switch (command)
+                {
+                    case "append":
+                        string word = inputList[1];
+                        ListProcessingMethods.Append(word, stringList);
+                        break;
+                    case "prepend":
+                        break;
+                    case "reverse":
+                        break;
+                    case "insert":
+                        break;
+                    case "delete":
+                        break;
+                    case "roll":
+                        break;
+                    case "sort":
+                        break;
+                    case "count":
+                        break;
+                    default:
+                        Console.WriteLine("Error: invalid command");
+                        break;
+                }
+
+                input = Console.ReadLine();
+            }
+
+            Console.WriteLine("Finished");
+        }
+    }
+}
